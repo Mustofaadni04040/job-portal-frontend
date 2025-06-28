2a5e1cf01257c37a405ee85cb55ab383c0dcf4d0
mustofa adny <adniadni04@gmail.com>
2025-06-28 18:54:30 +0700

    responsive: footer section & update responsive in carousel page

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
   return (
     <div className="w-full border-t border-slate-200">
       <div className="max-w-5xl mx-auto">
-        <div className="grid grid-cols-3 w-full justify-between py-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full justify-between px-5 md:px-0 py-10">
           <div className="max-w-96 mb-7">
             <img src="/logo-job.png" alt="logo" className="w-28 h-auto" />
             <p className="mt-3 text-sm text-slate-500">
@@ -21,7 +21,7 @@ export default function Footer() {
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <h2 className="text-lg font-medium mb-2">About Seeker</h2>
+            <h2 className="text-lg font-medium md:mb-2">About Seeker</h2>
             <div className="text-slate-500">
               <Link to="/" className="hover:text-primary">
                 Beranda
@@ -34,7 +34,7 @@ export default function Footer() {
             </div>
           </div>
           <div>
-            <h2 className="text-lg font-medium mb-2">Call Us</h2>
+            <h2 className="text-lg font-medium mb-2 mt-5 md:mt-0">Call Us</h2>
             <div className="text-slate-500">
               Jl. Menteng Raya No.62, RT.3/RW.9, Kb. Sirih, Kec. Menteng, Kota
               Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10340
