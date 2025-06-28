ec8d9b6e5e3786bc0e70424da5c910754dcba64b
mustofa adny <adniadni04@gmail.com>
2025-06-28 18:26:27 +0700

    responsive: hero section

--- src/components/fragments/homePage/HeroSection.jsx
+++ src/components/fragments/homePage/HeroSection.jsx
@@ -1,15 +1,15 @@
 export default function HeroSection() {
   return (
-    <div className="max-w-7xl mx-auto my-14">
+    <div className="px-5 w-full md:max-w-7xl mx-auto my-14">
       <div className="text-center">
-        <h1 className="text-[42px] font-bold">
+        <h1 className="text-4xl md:text-[42px] font-bold">
           Unlock Your{" "}
           <span className="bg-gradient-to-r from-primary to-[#e7407d] bg-clip-text text-transparent">
             Career Potential
           </span>{" "}
           With Seeker
         </h1>
-        <p className="text-md text-slate-500">
+        <p className="mt-3 text-sm md:text-base text-slate-500">
           Browse thousands of career opportunities tailored for you.
         </p>
       </div>
