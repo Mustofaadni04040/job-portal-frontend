24b0a9baabfbd74f3743599a7a6fcf4c7c681e28
mustofa adny <adniadni04@gmail.com>
2025-07-08 12:18:41 +0700

    style: update style ui not found in browse jobs page

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -62,9 +62,9 @@ export default function Browse() {
           <img
             src="/not-found-logo.png"
             alt="not-found-logo"
-            className="w-96 h-96"
+            className="w-80 h-80 md:w-96 md:h-96"
           />
-          <p className="text-center text-xl">
+          <p className="text-center text-sm md:text-base lg:text-xl">
             Maaf, pekerjaan yang kamu cari belum ada nih 🙏😊
           </p>
           <Button
